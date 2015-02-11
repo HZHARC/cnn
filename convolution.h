46fadab14670ecc7d8a2abeed73b173d4d421e77
HuangZonghao <huangzonghao@yeah.net>
2015-02-11 20:04:12 +0800

    including the codes

---
+++ convolution.h
@@ -0,0 +1,25 @@
+#ifndef _CONVOLUTION_H
+#define _CONVOLUTION_H
+#include "timer.h"
+#include "utils.h"
+#include <vector>
+
+/* some constants definition */
+#define IMG_HEIGHT 240
+#define IMG_WIDTH 320
+#define IMG_SIZE 76800
+
+#define BLKNUM 1500   // should be able to read from inernal function directly
+
+#define KERNEL_HEIGHT 7
+#define KERNEL_WIDTH 7
+#define KERNEL_SIZE 49
+
+
+
+using namespace std;
+
+
+
+
+#endif
